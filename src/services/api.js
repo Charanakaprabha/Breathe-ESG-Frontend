cd02029f5fd4afb6d622c72e48139fd673ddecaf
Charanakaprabha <Charanakaprabha@users.noreply.github.com>
2026-05-27 20:10:43 +0530

    Implement X-Device-ID client-side interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -8,4 +8,20 @@ const api = axios.create({
   }
 });
 
+// Attach a unique X-Device-ID header for client-side data isolation
+api.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined' && window.localStorage) {
+    let deviceId = localStorage.getItem('esg_device_id');
+    if (!deviceId) {
+      // Generate a simple, unique ID
+      deviceId = 'dev_' + Math.random().toString(36).substring(2, 15) + '_' + Date.now().toString(36);
+      localStorage.setItem('esg_device_id', deviceId);
+    }
+    config.headers['X-Device-ID'] = deviceId;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default api;
